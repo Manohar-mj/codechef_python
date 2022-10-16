6c86639b6deee6a81daad33b640ca2823c4bc1f8
tchala_coder <54686214+tchalacoder@users.noreply.github.com>
2022-10-16 13:35:21 +0530

    Create NUMGAME.cpp

---
+++ NUMGAME.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n%2==0) cout <<"ALICE"<< endl;
+	    else cout<<"BOB"<<endl;
+	}
+	return 0;
+}
