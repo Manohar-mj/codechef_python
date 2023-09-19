8854b163e01eb1586e5e509a9a08be4eae66489f
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2023-09-19 22:39:27 +0530

    Update Input.java

--- Codechef_java/Input.java
+++ Codechef_java/Input.java
@@ -10,3 +10,32 @@ class Codechef
 		System.out.println(varName);
 	}
 }
+
+
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// update your code below this line
+		Scanner obj=new Scanner(System.in);
+		int val=obj.nextInt();
+		System.out.println(val*4);
+
+
+
+	}
+}
+
+
+
+
+You have already seen how to read a String. Let's see how to read the other datatype values.
+
+nextInt(): Reads integer values.
+nextFloat(): Reads float value.
+nextDouble(): Reads double value.
+nextBoolean(): Reads boolean value.
+nextLine(): Reads string value until newline character ('\n') is encountered.
+next(): Reads string value until space or newline character is encountered.
