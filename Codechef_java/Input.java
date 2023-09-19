5fb743c4edfb905068d061fd53ec9b1ffb594dc0
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2023-09-19 22:36:24 +0530

    Create Input.java

---
+++ Codechef_java/Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner objName = new Scanner(System.in);
+		String varName = objName.nextLine();
+		System.out.println(varName);
+	}
+}
